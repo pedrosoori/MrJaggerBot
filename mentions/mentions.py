05897745f49d91b7b7375fb251070e520875a40b
pedrosoori <61288220+pedrosoori@users.noreply.github.com>
2021-09-13 01:21:00 +0200

    Update mentions.py

--- mentions/mentions.py
+++ mentions/mentions.py
@@ -411,7 +411,7 @@ def reply_to_jagger():
 
             repo.update_file(path=file2.path, message="Update", content=str(last_tweet), sha=file2.sha)
             
-def reply_to_jagger():
+def reply_to_ibai():
     #print('buscando ultimo tweet de MrJagger', flush=True)
     last_tweet = retrieve_last_seen_id(FILE_NAME2)
     tweet=api2.GetUserTimeline(screen_name='@IbaiLlanos', count=1, include_rts=False, exclude_replies=True)
@@ -419,7 +419,7 @@ def reply_to_jagger():
         if last_tweet != tweets.id:
             last_tweet = tweets.id
             store_last_seen_id(last_tweet, FILE_NAME2)
-            print('jagger ha subido tweet')
+            print('ibai ha subido tweet')
             print('respondiendo')
             
             try:
@@ -607,5 +607,5 @@ def reply_to_ooc():
 while True:
     reply_to_tweets()
     reply_to_jagger()
-    #reply_to_ooc()
+    reply_to_ibai()
     time.sleep(20)
